9f82d1836bab98a9fa36058438e0806f6cbb071a
hongngoc2003 <nhngoc1506@gmail.com>
2024-11-24 01:21:45 +0700

    add func: switch ui with keyboard

--- Assets/Scripts/UI/UI.cs
+++ Assets/Scripts/UI/UI.cs
@@ -1,23 +1,50 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor;
 using UnityEngine;
 
-public class UI : MonoBehaviour
-{
+public class UI : MonoBehaviour {
     [SerializeField] private GameObject characterUI;
+    [SerializeField] private GameObject skillTreeUI;
+    [SerializeField] private GameObject craftUI;
+    [SerializeField] private GameObject optionUI;
+
+
     public UIItemTooltip itemTooltip;
     public UIStatTooltip statTooltip;
 
     private void Start() {
-        
+        SwitchTo(null);
+
+        itemTooltip.gameObject.SetActive(false);
+        statTooltip.gameObject.SetActive(false);
+    }
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.C)) 
+            SwitchWithKeyTo(characterUI);
+
+        if (Input.GetKeyDown(KeyCode.B))
+            SwitchWithKeyTo(craftUI);
+
+        if (Input.GetKeyDown(KeyCode.K))
+            SwitchWithKeyTo(skillTreeUI);
+
+        if (Input.GetKeyDown(KeyCode.O))
+            SwitchWithKeyTo(optionUI);
+
     }
     public void SwitchTo(GameObject _menu) {
         for (int i = 0; i < transform.childCount; i++) {
             transform.GetChild(i).gameObject.SetActive(false);
         }
 
-        if(_menu != null)
+        if (_menu != null)
             _menu.SetActive(true);
+    }
+
+    public void SwitchWithKeyTo(GameObject _menu) {
+        if (_menu != null && _menu.activeSelf) {
+            _menu.SetActive(false);
+            return;
+        }
+
+        SwitchTo(_menu);
     }
 }
