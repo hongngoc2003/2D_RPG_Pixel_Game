52a9e317969d8a05c0197f1746cbc2c328822999
hongngoc2003 <nhngoc1506@gmail.com>
2024-12-22 20:41:46 +0700

    fix mainmenu

--- Assets/Scripts/UI/UIMainMenu.cs
+++ Assets/Scripts/UI/UIMainMenu.cs
@@ -8,6 +8,7 @@ public class UIMainMenu : MonoBehaviour
     [SerializeField] private GameObject continueButton;
     [SerializeField] UIFadeScreen fadeScreen;
     [SerializeField] private GameObject chooseLevel;
+    [SerializeField] private GameObject settingsUI;
     private GameData data;
     private void Start() {
         if(SaveManager.instance.HasNoSaveData())
@@ -27,14 +28,12 @@ public class UIMainMenu : MonoBehaviour
     public void ChooseCastle() => SceneManager.LoadScene("Castle");
     public void ChooseForest() => SceneManager.LoadScene("Forest");
 
+    public void ExitButton() => settingsUI.SetActive(false);
     public void ExitGame() {
 
     }
 
-    public void Settings() {
-        
-    }
-
+    public void Settings() => settingsUI.SetActive(true);
     IEnumerator LoadSceneWithFadeEffect(float _delay,string _sceneName) {
         fadeScreen.FadeOut();
         yield return new WaitForSeconds(_delay);
